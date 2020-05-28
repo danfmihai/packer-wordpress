5609f1e61f0574fbdc581c3d297aafb697e5f8d5
Mihai <danfmihai@hotmail.com>
2020-05-28 16:48:28 -0400

    edited

--- scripts/install-wp.sh
+++ scripts/install-wp.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-            sudo yum update -y
+            #sudo yum update -y
             sudo yum install -y httpd mariadb mariadb-server php php-common php-mysql php-gd php-xml php-mbstring php-mcrypt php-xmlrpc unzip wget
             sudo systemctl start httpd
             sudo systemctl start mariadb
             sudo systemctl enable httpd
             sudo systemctl enable mariadb
-            echo "<h1>This is the default website</h1>" > /var/www/html/index.html
+            #echo "<h1>This is the default website</h1>" > /var/www/html/index.html
             mysql -u root -e "CREATE DATABASE wordpress;"
             mysql -u root -e "GRANT ALL PRIVILEGES on wordpress.* to 'wp_user'@'localhost' identified by 'wordpress';"
             mysql -u root -e "FLUSH PRIVILEGES;"
+            mysql -u root -e "show databases;" 
+            echo
             echo "Database setup done!"
             echo
-            echo "Installing Wordpress...\"
+            echo "Installing Wordpress..."
+            echo
             sudo wget http://wordpress.org/latest.tar.gz
             sudo tar -xzvf latest.tar.gz
             sudo rm -rf wordpress*
@@ -23,10 +26,6 @@
             sudo chmod -R 755 /var/www/html/
             cd /var/www/html/
             sed -e "s/database_name_here/"wordpress"/" -e "s/username_here/"wp_user"/" -e "s/password_here/"wordpress"/" wp-config-sample.php > wp-config.php
-            cat <<EOF
-            ##############
-            WORDPRESS IS INSTALLED!
-            ##############
-            EOF
-            echo "Please verify your install and go to http://ip"
+            echo "Please verify your install and go to http://your-ip"
+            echo
 exit
